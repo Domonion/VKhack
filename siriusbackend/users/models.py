20a79d03fc3dbe6e5da3c9e83b369cb3d3cf4f09
Georgiy Nazarov <geranazavr555@yandex.ru>
2018-11-09 18:50:15 +0300

    Add model for users

--- siriusbackend/users/models.py
+++ siriusbackend/users/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    vk_id = models.BigIntegerField(db_index=True, primary_key=True)
+    banned = models.BooleanField(default=False)
+    spent_time = models.FloatField(default=0.0)
