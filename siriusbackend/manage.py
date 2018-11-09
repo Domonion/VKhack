05780b6355e23a5eb21d794f2e8199d389f2806a
vitalya <Vitaliy.ovechkin.99@mail.ru>
2018-11-10 02:45:37 +0300

    remove GRAPH_PATH

--- siriusbackend/manage.py
+++ siriusbackend/manage.py
@@ -5,12 +5,13 @@ import sys
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'siriusbackend.settings')
 
-    GRAPH_PATH = os.environ['GRAPH_PATH']
+    HOME = os.environ['HOME']
+    GRAPH_DIR = 'data'
     SUBCATEGORIES_GRAPH_FILE = 'subcategories_graph.json'
     EVENTS_GRAPH_FILE = 'events_graph.json'
 
-    SUBCATEGORIES_GRAPH = os.path.join(GRAPH_PATH, SUBCATEGORIES_GRAPH_FILE)
-    EVENTS_GRAPH = os.path.join(GRAPH_PATH, EVENTS_GRAPH_FILE)
+    SUBCATEGORIES_GRAPH = os.path.join(HOME, GRAPH_DIR, SUBCATEGORIES_GRAPH_FILE)
+    EVENTS_GRAPH = os.path.join(HOME, GRAPH_DIR, EVENTS_GRAPH_FILE)
 
     try:
         from django.core.management import execute_from_command_line
