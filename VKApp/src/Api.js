747cfa72bcb34e59b56f0611ed85da5093bbfc5f
Maksim <macsim.1999@gmail.com>
2018-11-10 00:37:13 +0300

    token test

--- VKApp/src/Api.js
+++ VKApp/src/Api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import connect from '@vkontakte/vkui-connect';
 
 export default class Api {
     static myInstance = null;
@@ -9,6 +10,24 @@ export default class Api {
         }
         return this.myInstance;
     };
+
+    getAccessToken() {
+        return new Promise(function (token, reject) {
+            connect.subscribe((e) => {
+                switch (e.detail.type) {
+                    case 'VKWebAppAccessTokenReceived':
+                        token(e.detail.data.accessToken);
+                        break;
+                    case 'VKWebAppAccessTokenFailed':
+                        reject(e.detail.data);
+                        break;
+                    default:
+                        console.log(e.detail.type);
+                }
+            });
+            connect.send("VKWebAppGetAuthToken", {"app_id": 6746736, "scope": "notify,friends"});
+        })
+    }
 
     getTags() {
         return new Promise(function(resolve, reject) {
