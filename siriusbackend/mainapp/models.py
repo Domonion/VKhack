f28ec07f11328214b0af8fcdc6f02b54e09d20fb
Georgiy Nazarov <geranazavr555@yandex.ru>
2018-11-10 13:24:00 +0300

    fix

--- siriusbackend/mainapp/models.py
+++ siriusbackend/mainapp/models.py
@@ -158,9 +158,9 @@ class Event(models.Model):
         }
 
         if self.start_datetime is not None:
-            result["start_datetime"] = self.start_datetime.strftime("%Y-%m-%d'T'%H:%M:%S")
+            result["start_datetime"] = self.start_datetime.strftime("%Y-%m-%d'T'%H:%M:%S%z")
         if self.finish_datetime is not None:
-            result["finish_datetime"] = self.finish_datetime.strftime("%Y-%m-%d'T'%H:%M:%S")
+            result["finish_datetime"] = self.finish_datetime.strftime("%Y-%m-%d'T'%H:%M:%S%z")
         if self.week_day != 0:
             result["week_day"] = self.week_day
         if self.place_address != "":
