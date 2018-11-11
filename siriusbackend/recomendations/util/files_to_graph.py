14e8010b7fdf67e808c847d80ce2f110c78aa516
vitalya <Vitaliy.ovechkin.99@mail.ru>
2018-11-11 06:53:29 +0300

    fixN+4

--- siriusbackend/recomendations/util/files_to_graph.py
+++ siriusbackend/recomendations/util/files_to_graph.py
@@ -1,6 +1,6 @@
 import json
 import os
-from filelock import FileLock
+from filelock import Timeout, FileLock
 
 from mainapp import models
 
@@ -28,7 +28,6 @@ EVENT_SET_QUERIES = os.path.join(HOME, GRAPH_DIR, EVENT_SET_QUERIES_FILE)
 
 
 def read_file(file_path):
-    #with FileLock(file_path):
     file = open(file_path, 'r')
     try:
         graph = json.loads(file.read())
@@ -40,7 +39,7 @@ def read_file(file_path):
 
 
 def write_file(file_path, item):
-    with FileLock(file_path):
+    with FileLock(file_path, timeout=1):
         file = open(file_path, 'w')
         file.write(json.dumps(item, separators=(',', ':')))
         file.close()
